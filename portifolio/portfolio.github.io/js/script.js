9d4cab1399cde07104af753dab155e78a5e26816
Rafael Ribeiro <59180005+rafabookaa@users.noreply.github.com>
2024-01-10 16:30:50 -0300

    Delete portifolio/portfolio.github.io directory

--- portifolio/portfolio.github.io/js/script.js
+++
@@ -1,7 +0,0 @@
-import initmenuHamburguer from "./modules/menuHamburguer.js";
-import initscrollSuave from "./modules/scrollSuave.js";
-import initLinkMenu from "./modules/ativaLink.js";
-
-initmenuHamburguer();
-initscrollSuave();
-initLinkMenu();
